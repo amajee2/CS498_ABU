46d3875c1c1ee553552b307b9d8f5cb81a5cdca5
amajee2 <86686688+amajee2@users.noreply.github.com>
2022-02-21 01:27:55 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const mysql = require('mysql2');
+const express = require('express');
+const bodyParser = require('body-parser');
+var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'master',
+        password: 'cs498',
+        database: 'userdb',
+});
+connection.connect();
+// Init web app
+const app = express();
+var jsonParser = bodyParser.json();
+app.get('/greeting', (req, res) => {
+    res.send('Hello World!')
+})
+app.post('/register',jsonParser, (req, res) => {
+    console.log(req.body.username);
+    let u = req.body.username;
+    let query = `INSERT INTO Users(username) VALUES ("${u}")`;
+    connection.query(query,(e, r) => {
+        console.log(r);
+         if (e){
+                 console.log(e);
+        }
+        else{
+                res.json({'messge':'add success', 'username':r});
+                console.log(r);
+        }
+    });
+});
+
+app.get('/list', (req, res) => {
+    let query = 'SELECT * FROM Users'
+    let query_res = null;
+    db.query(
+        query,
+        (err, results, fields) => {
+            if (err)
+                console.log(err)
+            else {
+                query_res = results
+                console.log(results)
+                console.log(fields)
+            }
+        }
+    )
+
+    res.json(query_res)
+});
+app.post('/clear', () => {
+    let query = `DELETE FROM Users`;
+    connection.query(query,(e, r) => {
+        console.log(r);
+         if (e){
+                 console.log(e);
+        }
+        else{
+                console.log('delete success');
+        }
+    });
+});
+
+PORT = 80
+app.listen(PORT, () =>
+    console.log('Listening ' + PORT)
+)
